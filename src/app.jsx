9d8e5f791f66ee9cddb7604df3ab1ccd48a63907
tonivargas28 <antoniovargasgarcia21@gmail.com>
2024-11-30 11:07:51 +0100

    1 done

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,16 @@
 import { Navbar } from "./components/ui"
+import { Routes, Route } from "react-router-dom"
+import Home from "./components/pages/Home";
+
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   )
 }
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci ipsa, tenetur repudiandae laudantium impedit ratione placeat, suscipit consectetur deserunt tempora fugit. Quibusdam neque incidunt, consequuntur soluta impedit nulla architecto maxime.</p>
+        </div>
+    );
+}
+
+export default Home
